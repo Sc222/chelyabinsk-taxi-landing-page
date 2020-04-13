d1158089cfc89863c03d4289bd08571e23daebe3
balandin228 <55559705+balandin228@users.noreply.github.com>
2020-04-14 00:12:25 +0500

    -60 px  scroll

--- smoothScroll.js
+++ smoothScroll.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
             event.preventDefault();
             var hash = this.hash;
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
+                scrollTop: $(hash).offset().top - 60
             }, 1200, function(){
                 window.location.hash = hash;
             });
