2729e3f8771464eaf34f206f9092ded316d0b82e
balandin228 <55559705+balandin228@users.noreply.github.com>
2020-04-16 00:57:28 +0500

    configure php

--- mail.php
+++ mail.php
@@ -4,22 +4,22 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
+$name = $_POST['UserName'];
+$surname = $_POST['UserSurname']
+$phone = $_POST['PhoneNumber'];
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'taxi-chelyabinsk74@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '.j2pa3ucPOYV'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
+$mail->setFrom('taxi-chelyabinsk74@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('igorek.chelovek@gmail.com');     // Кому будет уходить письмо
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -28,13 +28,11 @@ $mail->addAddress('gomudusu@p33.org');     // Кому будет уходить
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->Subject = 'Заявка на звонок с сайта аренды автомобилей';
+$mail->Body    = '' .$name . ' ' .$UserSurname.' оставил заявку, его телефон ' .$phone. ;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
     echo 'Error';
-} else {
-    header('location: thank-you.html');
 }
 ?>
